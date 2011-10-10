bdd6f08b4074609ed332502bb1d5c39d0805025d
Ivan Kanakarakis <ivan.kanak@gmail.com>
2011-10-10 22:12:51 +0300

    menu scrolling along page

---
+++ js/menuscroll.js
@@ -0,0 +1,23 @@
+(function () {
+    window.onload = function () {
+        var menu = document.getElementById('menu');
+        var init = menu.offsetTop;
+        var fixed = false;
+
+        window.onscroll = function () {
+            if (!fixed && (menu.offsetTop - scrollTop() < 0)) {
+                menu.className = 'fixed';
+                menu.style.top = 0;
+                fixed = true;
+            } else if (fixed && scrollTop() <= init) {
+                menu.className = 'abs';
+                menu.style.top = init + 'px';
+                fixed = false;
+            }
+        };
+    };
+
+    function scrollTop() {
+        return document.body.scrollTop || document.documentElement.scrollTop;
+    }
+})();
